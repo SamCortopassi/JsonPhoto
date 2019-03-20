f94962cda7ca1928ba0fae10920092ba3117061c
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-03-20 15:58:11 -0500

    set up

---
+++ JsonPhoto/FlickerAPI.swift
@@ -0,0 +1,19 @@
+//
+//  FlickerAPI.swift
+//  JsonPhoto
+//
+//  Created by Samantha Cortopassi on 3/20/19.
+//  Copyright © 2019 Samantha Cortopassi. All rights reserved.
+//
+
+import Foundation
+
+enum Method: String {
+    case iterestingPhotos = "flicker.interestingness.getList"
+}
+
+struct FlickerAPI {
+    
+    private static let baseURLString = "https:api.flicker.com/services/rest"
+    
+}
---
+++ JsonPhoto/PhotosViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosViewController.swift
+//  JsonPhoto
+//
+//  Created by Samantha Cortopassi on 3/20/19.
+//  Copyright © 2019 Samantha Cortopassi. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
+
+
